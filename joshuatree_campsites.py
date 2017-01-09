5ec25a87398a2ca01fe3e3c0805e66ea59b4c0e8
Chris Hymes <chris.hymes@gmail.com>
2017-01-09 07:28:44 -0800

    Joshua Tree campsites

---
+++ joshuatree_campsites.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import argparse
+import copy
+import requests
+
+import urllib
+from urllib.parse import parse_qs
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+# Hardcoded list of campgrounds I'm willing to sleep at
+PARKS = {
+    '70953': 'BLACK ROCK CAMPGROUND',
+    '70951': 'COTTONWOOD CAMPGROUND',
+    '70952': 'INDIAN COVE CAMPGROUND'
+}
+
+# Sets the search location to joshua tree
+LOCATION_PAYLOAD = {
+    'currentMaximumWindow': '12',
+    'locationCriteria': 'joshua tree',
+    'interest': '',
+    'locationPosition': '',
+    'selectedLocationCriteria': '',
+    'resetAllFilters':    'false',
+    'filtersFormSubmitted': 'false',
+    'glocIndex':    '0',
+    'googleLocations':  'Joshua Tree, CA, USA|-116.31306610000001|34.134728||LOCALITY'
+}
+
+# Sets the search type to camping
+CAMPING_PAYLOAD = {
+    'resetAllFilters':  'false',
+    'filtersFormSubmitted': 'true',
+    'sortBy':   'RELEVANCE',
+    'category': 'camping',
+    'selectedState':    '',
+    'selectedActivity': '',
+    'selectedAgency':   '',
+    'interest': 'camping',
+    'usingCampingForm': 'true'
+}
+
+# Runs the actual search
+PAYLOAD = {
+    'resetAllFilters':   'false',
+    'filtersFormSubmitted': 'true',
+    'sortBy':   'RELEVANCE',
+    'category': 'camping',
+    'availability': 'all',
+    'interest': 'camping',
+    'usingCampingForm': 'false'
+}
+
+
+BASE_URL = "https://www.recreation.gov"
+UNIF_SEARCH = "/unifSearch.do"
+UNIF_RESULTS = "/unifSearchResults.do"
+
+def findCampSites(args):
+    payload = generatePayload(args['start_date'], args['end_date'])
+
+    content_raw = sendRequest(payload)
+    html = BeautifulSoup(content_raw, 'html.parser')
+    sites = getSiteList(html)
+    return sites
+
+def getNextDay(date):
+    date_object = datetime.strptime(date, "%Y-%m-%d")
+    next_day = date_object + timedelta(days=1)
+    return datetime.strftime(next_day, "%Y-%m-%d")
+
+def formatDate(date):
+    date_object = datetime.strptime(date, "%Y-%m-%d")
+    date_formatted = datetime.strftime(date_object, "%a %b %d %Y")
+    return date_formatted
+
+def generatePayload(start, end):
+    payload = copy.copy(PAYLOAD)
+    payload['arrivalDate'] = formatDate(start)
+    payload['departureDate'] = formatDate(end)
+    return payload
+
+def getSiteList(html):
+    sites = html.findAll('div', {"class": "check_avail_panel"})
+    results = []
+    for site in sites:
+        if site.find('a', {'class': 'book_now'}):
+            get_url = site.find('a', {'class': 'book_now'})['href']
+            # Strip down to get query parameters
+            get_query = get_url[get_url.find("?") + 1:] if get_url.find("?") >= 0 else get_url
+            if get_query:
+                get_params = parse_qs(get_query)
+                siteId = get_params['parkId']
+                if siteId and siteId[0] in PARKS:
+                    results.append("%s, Booking Url: %s" % (PARKS[siteId[0]], BASE_URL + get_url))
+    return results
+
+def sendRequest(payload):
+    with requests.Session() as s:
+        
+        s.get(BASE_URL + UNIF_RESULTS) # Sets session cookie
+        s.post(BASE_URL + UNIF_SEARCH, LOCATION_PAYLOAD) # Sets location to yosemite
+        s.post(BASE_URL + UNIF_SEARCH, CAMPING_PAYLOAD) # Sets search type to camping
+
+        resp = s.post(BASE_URL + UNIF_SEARCH, payload) # Runs search on specified dates
+        if (resp.status_code != 200):
+            raise Exception("failedRequest","ERROR, %d code received from %s".format(resp.status_code, BASE_URL + SEARCH_PATH))
+        else:
+            return resp.text
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_date", required=True, type=str, help="Start date [YYYY-MM-DD]")
+    parser.add_argument("--end_date", type=str, help="End date [YYYY-MM-DD]")
+
+    args = parser.parse_args()
+    arg_dict = vars(args)
+    if 'end_date' not in arg_dict or not arg_dict['end_date']:
+        arg_dict['end_date'] = getNextDay(arg_dict['start_date'])
+
+    sites = findCampSites(arg_dict)
+    if sites:
+        for site in sites:
+            print(site + \
+                "&arrivalDate={}&departureDate={}" \
+                .format(
+                        urllib.parse.quote_plus(formatDate(arg_dict['start_date'])),
+                        urllib.parse.quote_plus(formatDate(arg_dict['end_date']))))
+
